97f034f9a30d5492360ff8a1f9cc78dfe1a4b76c
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-06-09 18:11:28 +0700

    a

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
       <section className="bg-gradient-to-l pt-10 from-yellow-200 to-green-100">
         <main className="pb-16">
           <h3 className="font-Judson text-[32px] sm:text-[34px] md:text-[36px] lg:text-[38px] xl:text-[40px] font-bold text-center">SMP NEGERI 2 NGAJUM</h3>
-          <p className="text-[15px] md:text-[18px] lg:text-[22px] xl:text-[24px] font-normal text-center mt-8">
+          <p className="text-[15px] md:text-[18px] lg:text-[22px] xl:text-[24px] font-normal text-center mt-8 px-2">
             Jl. Sukorejo, Rt 02/Rw 06, Ds. Kesamben, Kec. Ngajum, Kab. Malang, Area Sawah, Kesamben, Kec. Ngajum, Kabupaten Malang, Jawa Timur 65164
           </p>
         </main>
