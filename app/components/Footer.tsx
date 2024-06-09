b036e68e208c0acab567a5027c725138fe1ab2a5
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-06-09 18:09:45 +0700

    feat: susunan acara

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,9 @@ function Footer() {
       <section className="bg-gradient-to-l pt-10 from-yellow-200 to-green-100">
         <main className="pb-16">
           <h3 className="font-Judson text-[32px] sm:text-[34px] md:text-[36px] lg:text-[38px] xl:text-[40px] font-bold text-center">SMP NEGERI 2 NGAJUM</h3>
-          <p className="text-[15px] md:text-[18px] lg:text-[22px] xl:text-[24px] font-normal text-center mt-8">Jl. Raya Sukoraharjo No.326, Blobo, Sukoharjo, Kec. Kepanjen, Kabupaten Malang, Jawa Timur 65163</p>
+          <p className="text-[15px] md:text-[18px] lg:text-[22px] xl:text-[24px] font-normal text-center mt-8">
+            Jl. Sukorejo, Rt 02/Rw 06, Ds. Kesamben, Kec. Ngajum, Kab. Malang, Area Sawah, Kesamben, Kec. Ngajum, Kabupaten Malang, Jawa Timur 65164
+          </p>
         </main>
         <footer className="bg-white py-4">
           <p className="text-[12px] text-center font-normal">
--- app/components/SusunanAcara.tsx
+++ app/components/SusunanAcara.tsx
@@ -1,16 +1,31 @@
+import Image from "next/image";
 import React from "react";
+import mainGraduation from "@/public/svg/main-graduation.svg";
 
 export default function SusunanAcara() {
   return (
     <React.Fragment>
       <section className="pt-20 bg-gradient-to-tr from-yellow-300 to-green-100">
-        <div className="max-w-6xl mx-auto">
-          <h4 className="font-Judson text-[28px] sm:text-[30px] md:text-[32px] lg:text-[34px] xl:text-[36px]">Susunan Acara</h4>
+        <div className="max-w-6xl mx-auto relative">
+          <h4 className="font-Judson text-[28px] sm:text-[30px] md:text-[32px] lg:text-[34px] xl:text-[36px] font-semibold">Susunan Acara</h4>
           <main className="font-Karla text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] ml-8 mt-6">
-            <p>01. </p>
-            <p>02. </p>
-            <p>03. </p>
+            <p>01. Pembukaan</p>
+            <p>02. Menyanyikan Lagu Indonesia Raya</p>
+            <p>03. Pembacaan ayat Suci Al Quran</p>
+            <p>04. Tari Topeng Grebeg Sabrang</p>
+            <p>05. Laporan Pendidikan</p>
+            <p>06. Sambutan perwakilan kelas 7 dan 8</p>
+            <p>07. Sambutan perwakilan kelas 9</p>
+            <p>08. Koor Kelas 9</p>
+            <p>09. Sambutan Wakil orangtua kelas 9</p>
+            <p>10. Sambutan Kepala Sekolah</p>
+            <p>11. Tari Gandrung Banyuwangi</p>
+            <p>12. Sambutan Pengawas SMP Dinas Pendidikan Kabupaten Malang</p>
+            <p>13. Pengukuhan Wisudawan</p>
+            <p>14. Doa</p>
+            <p>15. Penutup</p>
           </main>
+          <Image src={mainGraduation} alt="a" className="w-full " />
         </div>
       </section>
     </React.Fragment>
